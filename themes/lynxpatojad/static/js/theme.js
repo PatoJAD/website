59c2171e3a354dc5dabaf21053648bc44e9d232e
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-11-10 14:27:59 -0300

    Add service

--- themes/lynxpatojad/static/js/theme.js
+++ themes/lynxpatojad/static/js/theme.js
@@ -85,6 +85,7 @@ function scrollTo(element, to = 0, duration = 1000) {
     });
     animateScroll();
 };
+
 Math.easeInOutQuad = function(t, b, c, d) {
     t /= d / 2;
     if (t < 1) return c / 2 * t * t + b;
