dc57051ff6e605e34e2ca1f0fe61bf70cf00152b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-11-17 18:06:41 -0300

    Remove JS unuse

--- themes/lynxpatojad/static/js/theme.js
+++ themes/lynxpatojad/static/js/theme.js
@@ -1,74 +1,53 @@
-(function() {
-    "use strict";
-    window.onload = function() {
-        window.setTimeout(fadeout, 200);
+window.onscroll = function() {
+    var header_navbar = document.querySelector(".navbar-area");
+    var sticky = header_navbar.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+    } else {
+        header_navbar.classList.remove("sticky");
     }
+};
 
-    function fadeout() {
-        document.querySelector('.preloader').style.opacity = '0';
-        document.querySelector('.preloader').style.display = 'none';
-    }
-    window.onscroll = function() {
-        var header_navbar = document.querySelector(".navbar-area");
-        var sticky = header_navbar.offsetTop;
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-        } else {
-            header_navbar.classList.remove("sticky");
+let navbarToggler = document.querySelector(".navbar-toggler");
+
+navbarToggler.addEventListener('click', function() {
+    navbarToggler.classList.toggle("active");
+})
+
+tns({
+    container: '.client-logo-carousel',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    gutter: 15,
+    nav: false,
+    controls: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        540: {
+            items: 2,
+        },
+        768: {
+            items: 3,
+        },
+        992: {
+            items: 4,
         }
-    };
-    let navbarToggler = document.querySelector(".navbar-toggler");
-    navbarToggler.addEventListener('click', function() {
-        navbarToggler.classList.toggle("active");
-    })
-    tns({
-        container: '.client-logo-carousel',
-        autoplay: true,
-        autoplayButtonOutput: false,
-        mouseDrag: true,
-        gutter: 15,
-        nav: false,
-        controls: false,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            540: {
-                items: 2,
-            },
-            768: {
-                items: 3,
-            },
-            992: {
-                items: 4,
-            }
-        }
-    });
-    var wow = new WOW({
-        mobile: false
-    });
-    wow.init();
-    var cu = new counterUp({
-        start: 0,
-        duration: 2000,
-        intvalues: true,
-        interval: 100,
-    });
-    cu.start();
-    var elements = document.getElementsByClassName("portfolio-btn");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].onclick = function() {
-            var el = elements[0];
-            while (el) {
-                if (el.tagName === "BUTTON") {
-                    el.classList.remove("active");
-                }
-                el = el.nextSibling;
-            }
-            this.classList.add("active");
-        };
     }
-})();
+});
+
+var wow = new WOW();
+wow.init();
+
+var cu = new counterUp({
+    start: 0,
+    duration: 2000,
+    intvalues: true,
+    interval: 100,
+});
+cu.start();
 
 function scrollTo(element, to = 0, duration = 1000) {
     const start = element.scrollTop;
@@ -92,6 +71,7 @@ Math.easeInOutQuad = function(t, b, c, d) {
     t--;
     return -c / 2 * (t * (t - 2) - 1) + b;
 };
+
 document.querySelector('.scroll-top').onclick = function() {
     scrollTo(document.documentElement);
 }
