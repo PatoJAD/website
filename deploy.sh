b7e8386236ef27329c99d515123d47d07b209ad0
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-05-04 17:45:48 -0300

    Add dploy data

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+# build
+hugo
+
+# navigate into the build output directory
+cd public
+
+echo 'patojad.com.ar' >CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:JoaquinDecima/coderhouse-web.git main:gh-pages
+cd -
