a7f45c9d84a78ffc360e5a22b553be113a9013a5
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-05-24 10:13:35 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,5 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:PatoJAD/website.git master:gh-pages
+git push -f git@github.com:PatoJAD/website.git main:gh-pages
 cd -
